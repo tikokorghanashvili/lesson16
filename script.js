2b188c52120dc04ae8f5326e6476ca257f1370e5
tikokorghanashvili <101791266+tikokorghanashvili@users.noreply.github.com>
2022-05-09 15:50:41 +0400

    add

--- script.js
+++ script.js
@@ -1,22 +1,71 @@
-// xml http Requist
-function getusers(){
-    let Requist=newXMLHttprequist();
-    Requist.addeventlistener('load',render);
+// xml http requist
+function usersemail(page){
+    let requist=new XMLHttpRequest();
+    requist.addeventlistener('load',render);
+    requist.addeventlistener('error',renderr);
 
-    Requist.open('GET','https://jsonplaceholder.typicode.com/posts');
-
-    Requist.send();
+    requist.open('GET', 'https://reqres.in/api/users?page='+ page);
+    requist.send();
 }
+let currentPage=1;
+let totalPagesApi;
+
 function render(){
-    let response = this.responseText;
-    let responsedata = json.parse(response);
+    let response=this.responsetext;
+    let responsedata=JSON.parse(response);
 
-    let ul=document.createElement('ul');
-    responsedata.data.forEach(item => {
-        let li=document.createComment('li');
-        li.textContent=item.email;
-    });
+    var fragment=document.createDocumentFragment();
+    responsedata.data.forEach(item=>{
+        let img=document.createElement('img');
+        img.src=item.avatar;
+        img.classList.add('class','img-block');
+        let li=document.createElement('li');
 
-    console.log(responsedata);
+        let pemail=document.createElement('p');
+        pemail.textContent=item.email;
+        pemail.classList.add('class','pemaill');
+        li.classList.add('class','list');
+
+        li.appendChild(img);
+        li.appendChild(pemail);
+        fragment.appendChild(li);
+        }
+    )
+
+    document.getElementById('ul-list').innerHTML=' ';
+    document.getElementById('ul-list').appendChild(fragment);
+    totalPagesApi=responsedata.total_pages;
 }
-getusers();
+function errorRender(){
+    let error=document.createElement('p');
+    error.textContent='server error';
+    document.getElementById('section').appendChild(error);
+}
+document.getElementById('previous').addEventListener('click',function(){
+    if (currentPage==1){
+        return;
+    }
+    currentPage -=1;
+    usersemail(currentPage);
+})
+document.getElementById('next').addEventListener('click',function(){
+    if (currentPage==totalPagesApi){
+        return;
+    }
+    currentPage+=1;
+    usersemail(currentPage);
+})
+
+usersemail(currentPage);
+
+
+
+
+
+
+
+
+
+
+
+
